13fba0823e263f98d8bd064f394f867373d0ded1
Nikita <kabanov.n.v@mail.ru>
2021-07-23 12:51:56 +0300

    создание архитектуры проекта и первых методов display

---
+++ src/js/lib/core.js
@@ -0,0 +1,20 @@
+// корневой файл с основным методом init
+const $ = function(selector) {
+    return new $.prototype.init(selector); //f-я возвращает новый экземпляр объекта(ниже)
+};
+
+$.prototype.init = function(selector) {
+    if (!selector) {
+        return this; //если селектор не указан => пустой объект
+    }
+    Object.assign(this, document.querySelectorAll(selector)); //добавляем элементы с селектором в this
+    this.length = document.querySelectorAll(selector).length; //добавляем метод length к this
+    return this;
+};
+
+// т.к. $.prototype.init возвращает объект - можно расширить методы этого объекта строкой ниже
+$.prototype.init.prototype = $.prototype; //изначально прототип init относится только к возвращенному объекту(стр.12)
+
+window.$ = $; //записываем ф-ю глобально
+
+export default $;
---
+++ src/js/lib/lib.js
@@ -0,0 +1,6 @@
+// файл для объединения в библиотеку - "обогащение" функции $ методами
+import $ from './core';
+import './modules/display';
+
+
+export default $;
