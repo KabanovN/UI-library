88ff1ea1302ac8fb38a0fdc2fff87d0beec4babb
Nikita <kabanov.n.v@mail.ru>
2021-08-26 13:45:00 +0300

    финализация проекта

--- src/js/lib/components/dropdown.js
+++ src/js/lib/components/dropdown.js
@@ -4,7 +4,6 @@ $.prototype.dropdown = function() {
     // нам необходимо сравнить id триггера и data- меню => fadeToggle, если совпадают
     for (let i = 0; i < this.length; i++) {
         const id = $(this[i]).getAttr('id');
-        console.log(id);
         $(this[i]).click(() => {
             $(`[data-toggle-id = ${id}]`).fadeToggle(300);
         });
